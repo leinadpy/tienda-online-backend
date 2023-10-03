a876ba6a7463574244513b2895704065006efa16
Daniel Ojeda <leinadpy@gmail.com>
2023-10-03 08:58:55 -0300

    Documentation for product's entity, createDto and endpoints

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsNumber,
   IsOptional,
@@ -7,36 +8,77 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Name of the product',
+    example: 'T-Shirt',
+    minimum: 6,
+  })
   @IsString()
   @MinLength(6)
   name: string;
 
+  @ApiProperty({
+    description: 'Description of the product',
+    example: 'T-Shirt with a logo',
+  })
   @IsString()
   description: string;
 
+  @ApiProperty({
+    description: 'Price of the product',
+    example: 10.99,
+  })
   @IsNumber()
   @IsPositive()
   price: number;
 
+  @ApiProperty({
+    description: 'Size of the product',
+    example: 'M',
+  })
   @IsString()
   size: string;
 
+  @ApiProperty({
+    description: 'Color of the product',
+    example: 'red',
+  })
   @IsString()
   color: string;
 
+  @ApiProperty({
+    description: 'Material of the product',
+    example: 'cotton',
+  })
   @IsString()
   material: string;
 
+  @ApiProperty({
+    description: 'Stock of the product',
+    example: 10,
+  })
   @IsNumber()
   stock: number;
 
+  @ApiPropertyOptional({
+    description: 'Image url of the product',
+    example: 'https://www.image.com/image.png',
+  })
   @IsString()
   @IsOptional()
   image_url?: string;
 
+  @ApiProperty({
+    description: 'Category id of the product',
+    example: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+  })
   @IsString()
   category_id: string;
 
+  @ApiProperty({
+    description: 'Brand id of the product',
+    example: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+  })
   @IsString()
   brand_id: string;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,33 +11,56 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Product } from './entities/product.entity';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   private readonly logger: Logger = new Logger(ProductsController.name);
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The product has been successfully created.',
+    type: Product,
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOkResponse({
+    description: 'The products has been successfully retrieved.',
+    type: [Product],
+  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ description: 'The product has been successfully retrieved.' })
+  @ApiNotFoundResponse({ description: 'Product not found.' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiResponse({ description: 'The product has been successfully updated.' })
+  @ApiNotFoundResponse({ description: 'Product not found.' })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiResponse({ description: 'The product has been successfully deleted.' })
+  @ApiNotFoundResponse({ description: 'Product not found.' })
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }

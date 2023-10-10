77d2448fe4f27c2f92382e9ebb187c4e31010dd1
Gaston <gastonsalaya@gmail.com>
2023-10-10 16:50:41 -0300

    Api documentation for Categories and Brands.

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
+  @ApiProperty({
+    description: 'Name of the category',
+    example: "Women's Clothing - Tops - T-Shirts",
+  })
   @IsString()
   name: string;
 }
